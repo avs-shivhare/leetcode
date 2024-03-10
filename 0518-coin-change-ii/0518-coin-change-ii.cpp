d013ae07d7a5899f80583aace18741b4f4c6d3a2
Ashish Shivhare <162727837+avs-shivhare@users.noreply.github.com>
2024-03-10 20:20:23 +0530

    Time: 15 ms (43.14%), Space: 19.6 MB (51.89%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -20,42 +20,42 @@ public:
 
 
     //tabulation method
-    // int change(int target, vector<int>& coins) {
-    //     int n = coins.size();
-    //     vector<vector<int>> dp(n,vector<int>(target+1,0));
-    //     for(int i =0; i<= target;i++) {
-    //         if(i%coins[0] == 0) dp[0][i] = 1;
-    //         else dp[0][i] = 0;
-    //     }
-    //     for(int i =1; i<n; i++) {
-    //         for(int j =0; j<=target; j++) {
-    //             int notTake = dp[i-1][j];
-    //             int take = 0;
-    //             if(j>= coins[i]) take = dp[i][j-coins[i]];
-    //             dp[i][j] = take+notTake;
-    //         }
-    //     }
-    //     return dp[n-1][target];
-    // }
-
-
-    //space optimization
     int change(int target, vector<int>& coins) {
         int n = coins.size();
-        vector<int> prev(target+1,0),curr(target+1,0);
+        vector<vector<int>> dp(n,vector<int>(target+1,0));
         for(int i =0; i<= target;i++) {
-            if(i%coins[0] == 0) prev[i] = 1;
-            else prev[i] = 0;
+            if(i%coins[0] == 0) dp[0][i] = 1;
+            else dp[0][i] = 0;
         }
         for(int i =1; i<n; i++) {
             for(int j =0; j<=target; j++) {
-                int notTake = prev[j];
+                int notTake = dp[i-1][j];
                 int take = 0;
-                if(j>= coins[i]) take = curr[j-coins[i]];
-                curr[j] = take+notTake;
+                if(j>= coins[i]) take = dp[i][j-coins[i]];
+                dp[i][j] = take+notTake;
             }
-            prev = curr;
         }
-        return prev[target];
+        return dp[n-1][target];
     }
+
+
+    //space optimization
+    // int change(int target, vector<int>& coins) {
+    //     int n = coins.size();
+    //     vector<int> prev(target+1,0),curr(target+1,0);
+    //     for(int i =0; i<= target;i++) {
+    //         if(i%coins[0] == 0) prev[i] = 1;
+    //         else prev[i] = 0;
+    //     }
+    //     for(int i =1; i<n; i++) {
+    //         for(int j =0; j<=target; j++) {
+    //             int notTake = prev[j];
+    //             int take = 0;
+    //             if(j>= coins[i]) take = curr[j-coins[i]];
+    //             curr[j] = take+notTake;
+    //         }
+    //         prev = curr;
+    //     }
+    //     return prev[target];
+    //}
 };
