7db822f612c027e3dada68671945f40b5534c2a1
Ashish Shivhare <162727837+avs-shivhare@users.noreply.github.com>
2024-03-10 17:34:24 +0530

    Time: 47 ms (40.29%), Space: 20.2 MB (37.73%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+
+
+    //recursion method with memorization
     int f(int ind,int amount,vector<int> &coins,vector<vector<int>> &dp) {
         if(ind == 0) {
             if(amount%coins[ind] == 0) {
@@ -15,24 +18,52 @@ public:
         }
         return dp[ind][amount] = min(take,notTake);
     }
+
+
+    //Tabulation method
+    // int coinChange(vector<int>& coins, int amount) {
+    //     int n = coins.size();
+    //     vector<vector<int>> dp(n,vector<int>(amount+1,0));
+    //     for(int i =0;i<= amount; i++) {
+    //         if(i%coins[0] == 0) {
+    //             dp[0][i] = i/coins[0];
+    //         }
+    //         else dp[0][i] = 1e9;
+    //     }
+    //     for(int i =1; i<n; i++) {
+    //         for(int j =0; j<= amount; j++) {
+    //             int notTake = 0+dp[i-1][j];
+    //             int take = INT_MAX;
+    //             if(j>= coins[i]) take = 1+dp[i][j-coins[i]];
+    //             dp[i][j] = min(take,notTake);
+    //         }
+    //     }
+    //     int ans= dp[n-1][amount];
+    //     if(ans >= 1e9) return -1;
+    //     return ans;
+    // }
+
+    
+    //Space optimization method
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        vector<int> prev(amount+1,0), curr(amount+1,0);
         for(int i =0;i<= amount; i++) {
             if(i%coins[0] == 0) {
-                dp[0][i] = i/coins[0];
+                prev[i] = i/coins[0];
             }
-            else dp[0][i] = 1e9;
+            else prev[i] = 1e9;
         }
         for(int i =1; i<n; i++) {
             for(int j =0; j<= amount; j++) {
-                int notTake = 0+dp[i-1][j];
+                int notTake = 0+prev[j];
                 int take = INT_MAX;
-                if(j>= coins[i]) take = 1+dp[i][j-coins[i]];
-                dp[i][j] = min(take,notTake);
+                if(j>= coins[i]) take = 1+curr[j-coins[i]];
+                curr[j] = min(take,notTake);
             }
+            prev = curr;
         }
-        int ans= dp[n-1][amount];
+        int ans= prev[amount];
         if(ans >= 1e9) return -1;
         return ans;
     }
