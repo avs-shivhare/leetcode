66cfbf92e733e4f5b1d8d19984d3e60089169bba
Ashish Shivhare <162727837+avs-shivhare@users.noreply.github.com>
2024-03-30 22:22:52 +0530

    Time: 3 ms (97.22%), Space: 12.4 MB (70.09%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
+    //unordered map
+    // vector<int> singleNumber(vector<int>& nums) {
+    //     unordered_map<int,int> mpp;
+    //     vector<int> ans;
+    //     for(int i: nums) {
+    //         mpp[i]++;
+    //     }
+    //     for(auto i: mpp) {
+    //         if(i.second == 1) ans.push_back(i.first);
+    //     }
+    //     return ans;
+    // }
+
+    //bit manipulation 
     vector<int> singleNumber(vector<int>& nums) {
-        unordered_map<int,int> mpp;
-        vector<int> ans;
+        long long r = 0;
+        for(int i: nums) r = (r^i);
+        int one =0,zero =0;
+        int rightShift = (r & -r);
         for(int i: nums) {
-            mpp[i]++;
+            if(i & rightShift) one = (one^i);
+            else zero = (zero^i);
         }
-        for(auto i: mpp) {
-            if(i.second == 1) ans.push_back(i.first);
-        }
-        return ans;
+        return {one,zero};
     }
 };
