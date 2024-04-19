ba862f4686076be94e1da668a21c31547a73f4ce
Ashish Shivhare <162727837+avs-shivhare@users.noreply.github.com>
2024-04-19 16:22:41 +0530

    Time: 3 ms (95.18%), Space: 18.7 MB (94.07%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -11,20 +11,43 @@
  */
 class Solution {
 public:
-    void f(TreeNode* root,int target,vector<int> temp,vector<vector<int>> &ans) {
+    //optimal solution
+    void f(TreeNode* root,int target,vector<int> &temp,vector<vector<int>> &ans) {
         if(root->left == NULL && root->right == NULL) {
             temp.push_back(root->val);
             if(target-root->val == 0) ans.push_back(temp);
+            temp.pop_back();
             return;
         }
         temp.push_back(root->val);
         if(root->left) f(root->left,target-root->val,temp,ans);
         if(root->right) f(root->right,target-root->val,temp,ans);
+        temp.pop_back();
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>> ans;
+        vector<int> temp;
         if(root == NULL) return ans;
-        f(root,targetSum,{},ans);
+        f(root,targetSum,temp,ans);
         return ans;
     }
+    
+    //brute force solution
+    // void f(TreeNode* root,int target,vector<int> temp,vector<vector<int>> &ans) {
+    //     if(root->left == NULL && root->right == NULL) {
+    //         temp.push_back(root->val);
+    //         if(target-root->val == 0) ans.push_back(temp);
+    //         return;
+    //     }
+    //     temp.push_back(root->val);
+    //     if(root->left) f(root->left,target-root->val,temp,ans);
+    //     if(root->right) f(root->right,target-root->val,temp,ans);
+    // }
+    // vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+    //     vector<vector<int>> ans;
+    //     //vector<int> temp;
+    //     if(root == NULL) return ans;
+    //     f(root,targetSum,{},ans);
+    //     return ans;
+    // }
 };
