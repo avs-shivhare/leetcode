44e77407fd70c6294082b1da49292c2236ba3b83
Ashish Shivhare <162727837+avs-shivhare@users.noreply.github.com>
2024-04-21 15:15:16 +0530

    Time: 150 ms (37.59%), Space: 85.3 MB (95.56%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
+    // solution with time complexity is O(2nlogn)
+    // int maximumGap(vector<int>& nums) {
+    //     if(nums.size() <2) return 0;
+    //     set<int> st;
+    //     for(int i: nums) st.insert(i);
+    //     int ans = 0;
+    //     int prev = -1;
+    //     for(int i: st) {
+    //         if(prev == -1) {
+    //             prev = i;
+    //             continue;
+    //         }
+    //         ans = max(ans,i-prev);
+    //         prev = i;
+    //     }
+    //     return ans;
+    // }
+
+    //better solution with time complexity is O(nlogn)+O(n) and space complexity of O(1)
     int maximumGap(vector<int>& nums) {
         if(nums.size() <2) return 0;
-        set<int> st;
-        for(int i: nums) st.insert(i);
+        sort(nums.begin(),nums.end());
         int ans = 0;
-        int prev = -1;
-        for(int i: st) {
-            if(prev == -1) {
-                prev = i;
-                continue;
-            }
-            ans = max(ans,i-prev);
-            prev = i;
+        int n = nums.size();
+        for(int i =0; i<n-1; i++) {
+            ans = max(ans,nums[i+1]-nums[i]);
         }
         return ans;
     }
