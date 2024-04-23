a3f6c8fdacbcc45bf5b2e08578c9b4ff49c6303c
Ashish Shivhare <162727837+avs-shivhare@users.noreply.github.com>
2024-04-23 15:12:06 +0530

    Time: 46 ms (88.22%), Space: 23.2 MB (98.81%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
+    // bool isNStraightHand(vector<int>& hand, int k) {
+    //     priority_queue<int> q;
+    //     if(hand.size()%k != 0) return false;
+    //     sort(hand.begin(),hand.end());
+    //     while(hand.size()) {
+    //         int i =0;
+    //         while(i<hand.size() && q.size() < k) {
+    //             if(q.empty() || q.top()+1 == hand[i] || q.top()-1 == hand[i]) {
+    //                 q.push(hand[i]);
+    //                 hand.erase(hand.begin()+i);
+    //             }
+    //             else i++;
+    //         }
+    //         if(q.size() != k) return false;
+    //         while(!q.empty()) q.pop();
+    //     }
+    //     return true;
+    // }
+
     bool isNStraightHand(vector<int>& hand, int k) {
-        priority_queue<int> q;
+        vector<int> q;
         if(hand.size()%k != 0) return false;
         sort(hand.begin(),hand.end());
         while(hand.size()) {
             int i =0;
             while(i<hand.size() && q.size() < k) {
-                if(q.empty() || q.top()+1 == hand[i] || q.top()-1 == hand[i]) {
-                    q.push(hand[i]);
+                if(q.empty() || q.back()+1 == hand[i] || q.back()-1 == hand[i]) {
+                    q.push_back(hand[i]);
                     hand.erase(hand.begin()+i);
                 }
                 else i++;
             }
             if(q.size() != k) return false;
-            while(!q.empty()) q.pop();
+            q.clear();
         }
         return true;
     }
