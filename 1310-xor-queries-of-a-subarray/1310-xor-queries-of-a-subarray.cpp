7f92f7a9d7dd41762ec6d02884b7f958f886e24b
Ashish Shivhare <aashishshivhare97@gmail.com>
2024-05-17 15:53:18 +0530

    Time: 55 ms (86.05%), Space: 40.9 MB (99.86%) - LeetHub

---
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        int xr = 0;
+        int n = arr.size();
+        for(int i =0; i<n; i++) {
+            xr ^= arr[i];
+            arr[i] = xr;
+        }
+        int m = queries.size();
+        vector<int> ans(m,0);
+        for(int i =0; i<m; i++) {
+            int left = queries[i][0];
+            int right = queries[i][1];
+            int pre = 0;
+            if(left-1 >= 0) pre = arr[left-1];
+            ans[i] = pre^arr[right];
+        }
+        return ans;
+    }
+};
