2e29f63bec6aa659c43c1a998318ba05a3e152a1
Ashish Shivhare <aashishshivhare97@gmail.com>
2024-05-21 18:14:49 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (62.90%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,13 +1,39 @@
 class Solution {
 public:
+    // // 1st approach
+    // // subset by recursion or PowerSet methd
+    // void findSubSet(int ind,vector<vector<int>> &ans,vector<int> &temp,vector<int> &nums,int &n) {
+    //     if(ind == n) {
+    //         ans.push_back(temp);
+    //         return;
+    //     }
+    //     //include
+    //     temp.push_back(nums[ind]);
+    //     findSubSet(ind+1,ans,temp,nums,n);
+    //     temp.pop_back();
+    //     //exclude
+    //     findSubSet(ind+1,ans,temp,nums,n);
+    //     return;
+    // }
+    // vector<vector<int>> subsets(vector<int>& nums) {
+    //     vector<int> temp;
+    //     vector<vector<int>> ans;
+    //     int n = nums.size();
+    //     findSubSet(0,ans,temp,nums,n);
+    //     return ans;
+    // }
+
+
+    // 2nd approach
+    // using subset method 
+    // here i use bit manipulation method to find all the subset for give array
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
-        int subset = (1<<n);
         vector<vector<int>> ans;
-        for(int i =0; i<subset; i++) {
+        for(int i =0; i<(1<<n); i++) {
             vector<int> temp;
             for(int j = 0; j<n; j++) {
-                if((i&(1<<j))) temp.push_back(nums[j]);
+                if(i&(1<<j)) temp.push_back(nums[j]);
             }
             ans.push_back(temp);
         }
