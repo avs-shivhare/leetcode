bcfeee7804341f79f057bdd31ba1c58b9c12c4ac
Ashish Shivhare <aashishshivhare97@gmail.com>
2024-05-24 18:25:31 +0530

    Time: 41 ms (74.30%), Space: 50.9 MB (49.72%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    int f(int ind,vector<int> &nums,vector<int> &dp,int &n) {
-        if(ind == n-1) return true;
-        if(ind >= n) return false;
-        if(dp[ind] != -1) return dp[ind];
-        for(int i =1; i<=nums[ind]; i++) {
-            if(f(ind+i,nums,dp,n)) return dp[ind] = true;
-        }
-        return dp[ind] = false;
-    }
+    // solve using dp 
+    // int f(int ind,vector<int> &nums,vector<int> &dp,int &n) {
+    //     if(ind == n-1) return true;
+    //     if(ind >= n) return false;
+    //     if(dp[ind] != -1) return dp[ind];
+    //     for(int i =1; i<=nums[ind]; i++) {
+    //         if(f(ind+i,nums,dp,n)) return dp[ind] = true;
+    //     }
+    //     return dp[ind] = false;
+    // }
+    // bool canJump(vector<int>& nums) {
+    //     //if(nums[0] == 0) return false;
+    //     int n = nums.size();
+    //     vector<int> dp(n,-1);
+    //     return f(0,nums,dp,n); 
+    // }
+
+    //greedy method 
     bool canJump(vector<int>& nums) {
-        //if(nums[0] == 0) return false;
+        int maxi = 0;
         int n = nums.size();
-        vector<int> dp(n,-1);
-        return f(0,nums,dp,n); 
+        for(int i =0; i<n; i++) {
+            if(i> maxi) return false;
+            maxi = max(maxi,i+nums[i]);
+        } 
+        return true;
     }
 };
