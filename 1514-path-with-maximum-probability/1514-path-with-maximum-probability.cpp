d5559e519886a70806b4c5298c478215f5c06731
Ashish Shivhare <aashishshivhare97@gmail.com>
2024-08-27 12:21:48 +0530

    Time: 115 ms (92.90%), Space: 68.7 MB (92.50%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<pair<int,double>> adj[n];
-        for(int i =0; i<edges.size(); i++) {
-            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
-            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
-        }
-        // for(int i =0;i<n; i++) {
-        //     for(auto x: adj[i]) cout<<i<<"-> "<<x.first<<" "<<x.second<<endl;
-        // }
-        vector<bool> vis(n,false);
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succ, int start, int end) {
         priority_queue<pair<double,int>> q;
+        vector<pair<int,double>> adj[n];
+        int si = succ.size();
+        for(int i =0; i<si; i++) {
+            adj[edges[i][0]].push_back({edges[i][1],succ[i]});
+            adj[edges[i][1]].push_back({edges[i][0],succ[i]});
+        }
+        vector<double> ans(n,0.0);
+        ans[start] = 1;
         q.push({1,start});
         while(!q.empty()) {
+            double cost = q.top().first;
             int node = q.top().second;
-            double val = q.top().first;
+            //cout<<node<<" "<<cost<<endl;
             q.pop();
-            //cout<<node<<endl;
-            if(vis[node]) continue;
-            vis[node] = true;
-            if(node == end) {
-                //cout<<"done"<<endl;
-                return val;
-            }
+            if(node == end) return cost;
             for(auto i: adj[node]) {
-                int newNode = i.first;
-                double wt = i.second;
-                
-                q.push({val*wt,newNode});
-                
+                double temp = cost*i.second;
+                //cout<<"temp "<<i.first<<" "<<temp<<endl;
+                if(ans[i.first] < temp) {
+                    ans[i.first] = temp;
+                    q.push({temp,i.first});
+                }
             }
         }
-        return 0;
+        return 0.0;
     }
 };
