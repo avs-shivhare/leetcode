7b954ad711e5e086a0f55a218265cf01a8b41ac1
Ashish Shivhare <aashishshivhare97@gmail.com>
2024-08-31 13:37:42 +0530

    Time: 129 ms (42.83%), Space: 69 MB (66.72%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succ, int start, int end) {
-        priority_queue<pair<double,int>> q;
         vector<pair<int,double>> adj[n];
         int si = succ.size();
         for(int i =0; i<si; i++) {
             adj[edges[i][0]].push_back({edges[i][1],succ[i]});
             adj[edges[i][1]].push_back({edges[i][0],succ[i]});
-        }
-        vector<double> ans(n,0.0);
-        ans[start] = 1;
+        } 
+        priority_queue<pair<double,int>> q;
+        vector<double> dist(n,0);
+        dist[start] = 1;
         q.push({1,start});
         while(!q.empty()) {
-            double cost = q.top().first;
-            int node = q.top().second;
-            //cout<<node<<" "<<cost<<endl;
+            auto cost = q.top().first;
+            auto node = q.top().second;
             q.pop();
             if(node == end) return cost;
-            for(auto i: adj[node]) {
-                double temp = cost*i.second;
-                //cout<<"temp "<<i.first<<" "<<temp<<endl;
-                if(ans[i.first] < temp) {
-                    ans[i.first] = temp;
-                    q.push({temp,i.first});
+            for(auto x: adj[node]) {
+                double newCost = cost*x.second;
+                if(dist[x.first] < newCost) {
+                    dist[x.first] = newCost;
+                    q.push({newCost,x.first});
                 }
             }
         }
-        return 0.0;
+        return 0;
     }
 };
