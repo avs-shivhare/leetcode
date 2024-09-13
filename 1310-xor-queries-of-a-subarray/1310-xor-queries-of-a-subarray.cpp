a0a4a33c3800e30b838936d7906a74131296f512
Ashish Shivhare <aashishshivhare97@gmail.com>
2024-09-13 07:31:57 +0530

    Time: 65 ms (44.49%), Space: 44.8 MB (20.02%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        int xr = 0;
         int n = arr.size();
-        // precompute xor of every index
-        for(int i =0; i<n; i++) {
-            xr ^= arr[i];
-            arr[i] = xr;
+        vector<int> prefix(n+1,0);
+        int x = 0;
+        for(int i =1; i<=n; i++) {
+            x ^= arr[i-1];
+            prefix[i] = x;
         }
-        int m = queries.size();
-        vector<int> ans(m,0);
-        // finding answer of any queries by range precompute answer xor range answer
-        for(int i =0; i<m; i++) {
-            int left = queries[i][0];
-            int right = queries[i][1];
-            int pre = 0;
-            if(left-1 >= 0) pre = arr[left-1];
-            ans[i] = pre^arr[right];
+        vector<int> ans;
+        for(auto i: queries) {
+            int temp = prefix[i[1]+1];
+            temp ^= prefix[i[0]];
+            ans.push_back(temp);
         }
         return ans;
     }
