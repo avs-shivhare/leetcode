89de6f0daac95b2205447bcf9e231501c3bc2191
Ashish Shivhare <aashishshivhare97@gmail.com>
2024-09-21 08:52:50 +0530

    Time: 430 ms (5.05%), Space: 16.9 MB (34.93%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    static bool compareS(int x,int y) {
-        string t1 = to_string(x);
-        string t2 = to_string(y);
-        int val = t1.compare(t2);
-        if(val < 0) return true;
-        return false;
+    static bool compare(int &t1,int &t2) {
+        string s1 = to_string(t1);
+        string s2 = to_string(t2);
+        if(s1 > s2) return false;
+        return true;
     }
     vector<int> lexicalOrder(int n) {
         vector<int> ans;
-        for(int i =1; i<= n; i++) {
+        for(int i =1; i<=n; i++) {
             ans.push_back(i);
         }
-        sort(ans.begin(),ans.end(),compareS);
+        sort(ans.begin(),ans.end(),compare);
         return ans;
     }
 };
