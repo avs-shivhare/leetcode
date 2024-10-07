ce1b93123863e9bd1787f0896a7bae1b6cc5f37b
Ashish Shivhare <aashishshivhare97@gmail.com>
2024-10-07 12:18:09 +0530

    Time: 6 ms (57.32%), Space: 11.6 MB (61.24%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+        for(auto i: s) {
+            if(!st.empty() && i == 'B' && st.top() == 'A') {
+                st.pop();
+            }
+            else if(!st.empty() && i == 'D' && st.top() == 'C') {
+                st.pop();
+            }
+            else {
+                st.push(i);
+            }
+        }
+        return st.size();
+    }
+};
