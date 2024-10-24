1b1053668800d3830285b3d3bb2d4c306e12bde4
Ashish Shivhare <aashishshivhare97@gmail.com>
2024-10-24 08:33:32 +0530

    Time: 0 ms (100.00%), Space: 14.7 MB (21.11%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -11,27 +11,10 @@
  */
 class Solution {
 public:
-    int height(TreeNode* root) {
-        if(root == NULL) return 0;
-        int left = height(root->left);
-        int right = height(root->right);
-        return max(left,right)+1; 
-    }
-    bool find(TreeNode* root1,TreeNode* root2) {
-        if(root1 == NULL && root2 == NULL) return true;
-        if(root1== NULL || root2 == NULL) return false;
-        if(root1->val == root2->val) {
-            bool left = false,right = false;
-            left |= find(root1->left,root2->left);
-            left |= find(root1->right,root2->left);
-            right |= find(root1->right,root2->right);
-            right |= find(root1->left,root2->right);
-            return left && right;
-        }
-        return false;
-    }
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        if(height(root1) != height(root2)) return false;
-        return find(root1,root2);
+        if(root1 == NULL && root2 == NULL) return true;
+        if(root1 == NULL || root2 == NULL) return false;
+        if(root1->val != root2->val) return false;
+        return (flipEquiv(root1->left,root2->left) && flipEquiv(root1->right,root2->right)) || (flipEquiv(root1->left,root2->right) && flipEquiv(root1->right,root2->left)); 
     }
 };
