227bcea7cac18edc2c813501c6b671da14d65f6c
Ashish Shivhare <aashishshivhare97@gmail.com>
2025-05-14 18:10:56 +0530

    Time: 222 ms (79.17%), Space: 70.9 MB (62.5%) - LeetHub

--- 3630-total-characters-in-string-after-transformations-ii/3630-total-characters-in-string-after-transformations-ii.cpp
+++ 3630-total-characters-in-string-after-transformations-ii/3630-total-characters-in-string-after-transformations-ii.cpp
@@ -6,20 +6,20 @@ public:
         for(int i = 0; i<26; i++) ans[i][i] = 1;
         while(t) {
             if(t&1) {
-                ans = mutiply(a,ans);
+                ans = multiply(a,ans);
             }
-            a = mutiply(a,a);
+            a = multiply(a,a);
             t >>= 1;
         }
         return ans;
     }
-    vector<vector<long long>> mutiply(vector<vector<long long>> a,vector<vector<long long>> b) {
+    vector<vector<long long>> multiply(vector<vector<long long>> &a,vector<vector<long long>> &b) {
         vector<vector<long long>> ans(26,vector<long long>(26,0));
         for(int i = 0; i<26; i++) {
             for(int k = 0; k<26; k++) {
                 if(a[i][k] == 0) continue;
                 for(int j = 0; j<26; j++) {
-                    ans[i][j] = (ans[i][j]+a[i][k]*b[k][j])%mod;
+                    ans[i][j]= (ans[i][j]+a[i][k]*b[k][j])%mod;
                 }
             }
         }
@@ -28,7 +28,7 @@ public:
     int lengthAfterTransformations(string s, int t, vector<int>& nums) {
         vector<vector<long long>> tra(26,vector<long long>(26,0));
         for(int i = 0; i<26; i++) {
-            for(int j = 1; j<=nums[i]; j++) {
+            for(int j = 1; j<nums[i]+1; j++) {
                 tra[i][(i+j)%26]++;
             }
         }
@@ -37,14 +37,14 @@ public:
         for(auto i: s) {
             cnt[i-'a']++;
         }
-        vector<long long> temp(26,0);
+        vector<long long> fr(26,0);
         for(int i = 0; i<26; i++) {
             for(int j = 0; j<26; j++) {
-                temp[j] = (temp[j]+cnt[i]*tra[i][j])%mod;
+                fr[j] = (fr[j]+cnt[i]*tra[i][j])%mod;
             }
         }
         long long ans = 0;
-        for(auto i: temp) {
+        for(auto i: fr) {
             ans += i;
             ans %= mod;
         }
