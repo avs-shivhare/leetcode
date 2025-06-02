6b57c82862deaecd148ba4cb1545144faf5c02a7
Ashish Shivhare <aashishshivhare97@gmail.com>
2025-06-02 12:04:30 +0530

    Time: 4 ms (17.74%), Space: 23.3 MB (24.46%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> candy(n,1);
-        for(int i =1; i<n; i++) {
-            if(ratings[i] > ratings[i-1]) candy[i] = candy[i-1]+1;
+        vector<int> prefix(n,1),suffix(n,1);
+        for(int i = 1; i<n; i++) {
+            if(ratings[i] > ratings[i-1]) prefix[i] += prefix[i-1];
         }
-        int ans = 0;
+        int ans = max(prefix[n-1],suffix[n-1]);
         for(int i = n-2; i>=0; i--) {
-            if(ratings[i] > ratings[i+1]) {
-                candy[i] = max(candy[i],candy[i+1]+1);
-            }
-            ans +=candy[i];
+            if(ratings[i] > ratings[i+1]) suffix[i] += suffix[i+1];
+            ans += max(prefix[i],suffix[i]);
         }
-        ans += candy[n-1];
         return ans;
     }
 };
