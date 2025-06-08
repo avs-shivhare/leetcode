7b11e21dae3612d1cf6746a85c0ca604a8695f51
Ashish Shivhare <aashishshivhare97@gmail.com>
2025-06-08 13:57:29 +0530

    Time: 44 ms (20.57%), Space: 36.7 MB (5.05%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,17 +1,46 @@
+class Trie{
+    public:
+    vector<Trie*> next;
+    bool last;
+    Trie() {
+        next.resize(10,NULL);
+        last = false;
+    }
+};
 class Solution {
 public:
-    static bool compare(int &t1,int &t2) {
-        string s1 = to_string(t1);
-        string s2 = to_string(t2);
-        if(s1 > s2) return false;
-        return true;
+    vector<int> ans;
+    Trie* root = new Trie();
+    void add(string x) {
+        Trie* temp = root;
+        for(auto i: x) {
+            if(temp->next[i-'0'] == NULL) {
+                temp->next[i-'0'] = new Trie();
+            }
+            temp = temp->next[i-'0'];
+        }
+        temp->last = true;
+        return;
+    }
+    void search(Trie* root,string &x) {
+        if(root->last) {
+            ans.push_back(stoi(x));
+        }
+        for(int i = 0; i<10; i++) {
+            if(root->next[i]) {
+                x.push_back(i+'0');
+                search(root->next[i],x);
+                x.pop_back();
+            }
+        }
+        return;
     }
     vector<int> lexicalOrder(int n) {
-        vector<int> ans;
-        for(int i =1; i<=n; i++) {
-            ans.push_back(i);
+        for(int i = 1; i<=n; i++) {
+            add(to_string(i));
         }
-        sort(ans.begin(),ans.end(),compare);
+        string x = "";
+        search(root,x);
         return ans;
     }
 };
