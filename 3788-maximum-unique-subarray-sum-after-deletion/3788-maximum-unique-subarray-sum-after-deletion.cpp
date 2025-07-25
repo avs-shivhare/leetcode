30e264858ea669fe5741ace86c034e188bf508b4
Ashish Shivhare <aashishshivhare97@gmail.com>
2025-07-25 09:29:58 +0530

    Time: 0 ms (100%), Space: 29.7 MB (37.84%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int maxSum(vector<int>& nums) {
-        unordered_map<int,int> mpp;
         int sum = 0;
-        int ans = -1e9;
+        int maxi = -1e9;
+        unordered_map<int,int> mpp;
         for(auto i: nums) {
-            ans = max(ans,i);
-            if(i < 0) {
-
-            }
-            else if(mpp.count(i) == 0) {
+            if(i >= 0 && mpp.count(i) == 0) {
                 sum += i;
                 mpp[i]++;
             }
+            maxi = max(i,maxi);
         }
-        if(mpp.size() > 0) ans = max(ans,sum);
-        return ans;
+        if(sum == 0) return maxi;
+        return sum;
     }
 };
