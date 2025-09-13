dad43db1820605a9d23cfb9d73d4ab16d8474bed
Ashish Shivhare <aashishshivhare97@gmail.com>
2025-09-13 09:29:06 +0530

    Time: 3 ms (37.49%), Space: 9.8 MB (44.54%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
+        int cnt = 0,cnt2 = 0;
         unordered_map<char,int> mpp;
         for(auto i: s) {
             mpp[i]++;
         }
-        int v = 0,c = 0;
         for(auto i: mpp) {
-            if(i.first == 'a' || i.first == 'e' || i.first == 'i' || i.first == 'o' || i.first == 'u') {
-                v = max(v,i.second);
-            }
-            else {
-                c = max(c,i.second);
-            }
+            if(i.first == 'a' || i.first == 'e' || i.first == 'i' || i.first == 'o' || i.first == 'u') cnt = max(cnt,i.second);
+            else cnt2 = max(cnt2,i.second);
         }
-        return v+c;
+        return cnt+cnt2;
     }
 };
