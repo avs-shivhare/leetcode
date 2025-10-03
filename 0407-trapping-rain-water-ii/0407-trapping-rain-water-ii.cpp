2bb471caea505bf5f9cbfd11de3b84adaebb5fd3
Ashish Shivhare <aashishshivhare97@gmail.com>
2025-10-03 20:12:16 +0530

    Time: 79 ms (28.43%), Space: 24.5 MB (29.98%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
@@ -1,36 +1,55 @@
 class Solution {
 public:
-    int rowdiff[4] = {0,0,-1,1};
-    int coldiff[4] = {1,-1,0,0};
-    int trapRainWater(vector<vector<int>>& nums) {
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> q;
-        int n = nums.size();
-        int m = nums[0].size();
+    int trapRainWater(vector<vector<int>>& arr) {
+        int rowdiff[4] = {0,0,1,-1};
+        int coldiff[4] = {1,-1,0,0};
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> q;
+        int n = arr.size();
+        int m = arr[0].size();
+        vector<vector<int>> dist = arr;
         vector<vector<bool>> vis(n,vector<bool>(m,false));
         for(int i = 0; i<n; i++) {
             for(int j = 0; j<m; j++) {
-                if(i == 0 || j == 0 || i == n-1 || j == m-1) {
-                    q.push({nums[i][j],{i,j}});
+                if((i == 0 || j == 0 || j == m-1 || i == n-1) && !vis[i][j]) {
+                    //cout<<i<<" "<<j<<endl;
+                    q.push({arr[i][j],i,j});
                     vis[i][j] = true;
+                    //dist[i][j] = arr[i][j];
+                }
+            }
+        }
+        while(!q.empty()) {
+            int val = q.top()[0];
+            int r = q.top()[1];
+            int c = q.top()[2];
+            //cout<<r<<" "<<c<<" -> "<<val<<endl;
+            q.pop();
+            for(int i = 0; i<4; i++) {
+                int nr = r+rowdiff[i];
+                int nc = c+coldiff[i];
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && !vis[nr][nc]) {
+                    //cout<<nr<<" "<<nc<<" "<<val<<endl;
+                    dist[nr][nc] = max(dist[nr][nc],val);
+                    vis[nr][nc] = true;
+                    q.push({dist[nr][nc],nr,nc});
                 }
             }
         }
         int ans = 0;
-        while(!q.empty()) {
-            auto cost = q.top().first;
-            auto r = q.top().second.first;
-            auto c = q.top().second.second;
-            q.pop();
-            for(int i = 0; i<4; i++) {
-                int newrow = r+rowdiff[i];
-                int newcol = c+coldiff[i];
-                if(newrow >= 0 && newrow < n && newcol >= 0 && newcol < m && !vis[newrow][newcol]) {
-                    if(cost-nums[newrow][newcol] > 0) ans += cost-nums[newrow][newcol];
-                    q.push({max(cost,nums[newrow][newcol]),{newrow,newcol}});
-                    vis[newrow][newcol] = true;
-                }
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<m; j++) {
+                //cout<<dist[i][j]<<" ";
+                ans += dist[i][j]-arr[i][j];
             }
+            //cout<<endl;
         }
+        // for(int i = 0; i<n; i++) {
+        //     for(int j = 0; j<m; j++) {
+        //         cout<<vis[i][j]<<" ";
+        //         ans += dist[i][j]-arr[i][j];
+        //     }
+        //     cout<<endl;
+        // }
         return ans;
     }
 };
