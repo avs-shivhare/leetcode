5841d24645a6d91e8407852bffda6799ce02b2f5
Ashish Shivhare <aashishshivhare97@gmail.com>
2025-10-10 13:23:12 +0530

    Time: 145 ms (40.49%), Space: 153 MB (16.2%) - LeetHub

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int maximumEnergy(vector<int>& energy, int k) {
         int n = energy.size();
-        unordered_map<int,int> mpp;
+        vector<int> dp(n+k,0);
         int ans = -1e9;
         for(int i = n-1; i>=0; i--) {
-            int sum = energy[i]+mpp[i+k];
-            mpp[i] = sum;
+            int sum = energy[i]+dp[i+k];
+            dp[i] = sum;
             //cout<<i<<" "<<sum<<endl;
-            ans = max(ans,mpp[i]);
+            ans = max(ans,dp[i]);
         }
         return ans;
     }
