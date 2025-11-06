2fb71f94a9b3212cf04796e9c0516930f5a75abc
Ashish Shivhare <aashishshivhare97@gmail.com>
2025-11-06 16:45:07 +0530

    Time: 525 ms (27.31%), Space: 392 MB (47.59%) - LeetHub

--- 3863-power-grid-maintenance/3863-power-grid-maintenance.cpp
+++ 3863-power-grid-maintenance/3863-power-grid-maintenance.cpp
@@ -1,54 +1,52 @@
-class DSU{
+class DisJoinSet {
     public:
-    vector<int> parent,size;
-    DSU(int n) {
-        parent.resize(n+1,0);
-        size.resize(n+1,1);
+    vector<int> size,parent;
+    DisJoinSet(int n) {
+        size.resize(n,1);
+        parent.resize(n,0);
         iota(parent.begin(),parent.end(),0);
     }
-    int Uparent(int n) {
-        if(parent[n] == n) return n;
-        return parent[n] = Uparent(parent[n]);
-    }
     void add(int a,int b) {
-        int Ua = Uparent(a);
-        int Ub = Uparent(b);
-        if(Ua == Ub) return;
-        if(size[Ua] > size[Ub]) {
-            parent[Ub] = Ua;
-            size[Ua] += size[Ub];
+        int ua = findParent(a);
+        int ub = findParent(b);
+        if(ua == ub) return;
+        if(size[ua] > size[ub]) {
+            parent[ub] = ua;
+            size[ua] += size[ub];
         }
         else {
-            parent[Ua] = Ub;
-            size[Ub] += size[Ua];
+            parent[ua] = ub;
+            size[ub] += size[ua];
         }
         return;
+    }
+    int findParent(int n) {
+        if(parent[n] == n) return n;
+        return parent[n] = findParent(parent[n]);
     }
 };
 class Solution {
 public:
-    vector<int> processQueries(int c, vector<vector<int>>& edges, vector<vector<int>>& queries) {
-        DSU u(c);
-        for(auto i: edges) {
-            u.add(i[0],i[1]);
+    vector<int> processQueries(int n, vector<vector<int>>& edges, vector<vector<int>>& q) {
+        DisJoinSet ds(n+1);
+        for(auto  i: edges) {
+            ds.add(i[0],i[1]);
         }
-        unordered_map<int,set<int>> mpp;
-        for(int i = 1; i<=c; i++) {
-            mpp[u.Uparent(i)].insert(i);
+        map<int,set<int>> mpp;
+        for(int i = 1; i<=n; i++) {
+            mpp[ds.findParent(i)].insert(i);
         }
         vector<int> ans;
-        for(auto i: queries) {
+        for(auto i: q) {
             if(i[0] == 1) {
-                int parent = u.Uparent(i[1]);
-                if(mpp[parent].count(i[1])) ans.push_back(i[1]);
-                else if(mpp[parent].empty()) ans.push_back(-1);
-                else {
-                    ans.push_back(*(mpp[parent].begin()));
-                }
+                int par = ds.findParent(i[1]);
+                if(mpp[par].find(i[1]) != mpp[par].end()) ans.push_back(i[1]);
+                else if(!mpp[par].empty()) ans.push_back(*(mpp[par].begin()));
+                else ans.push_back(-1);
             }
             else {
-                int parent = u.Uparent(i[1]);
-                mpp[parent].erase(i[1]);
+                int par = ds.findParent(i[1]);
+                mpp[par].erase(i[1]);
             }
         }
         return ans;
