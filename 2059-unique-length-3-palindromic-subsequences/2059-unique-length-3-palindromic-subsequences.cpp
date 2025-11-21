cb81207498ad0c5e657b7783d2ca37f24f4c1896
Ashish Shivhare <aashishshivhare97@gmail.com>
2025-11-21 12:56:00 +0530

    Time: 446 ms (13.04%), Space: 16.2 MB (33.71%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -2,31 +2,21 @@ class Solution {
 public:
     int countPalindromicSubsequence(string s) {
         int n = s.size();
-        int prefix[100001][26];
-        memset(prefix,0,sizeof(prefix));
-        int frq[26];
-        unordered_map<char,vector<int>> mpp;
-        int ans = 0;
-        memset(frq,0,sizeof(frq));
-        for(int i =  1; i<=n; i++) {
-            frq[s[i-1]-'a']++;
-            for(int j = 0; j<26; j++) prefix[i][j] = frq[j];
-            if(mpp.count(s[i-1])) {
-                if(mpp[s[i-1]].size() == 2) mpp[s[i-1]][1] = i-1;
-                else mpp[s[i-1]].push_back(i-1);
-            }
-            else mpp[s[i-1]].push_back(i-1);
+        vector<int> prefix(26,0),suffix(26,0);
+        unordered_map<char,set<int>> mpp;
+        for(int i = 0; i<n; i++) {
+            suffix[s[i]-'a']++;
         }
-        for(auto i: mpp) {
-            if(i.second.size() < 2) continue;
-            int low = i.second[0];
-            int high = i.second[1]+1;
+        int ans = 0;
+        for(int i = 0; i<n; i++) {
+            suffix[s[i]-'a']--;
             for(int j = 0; j<26; j++) {
-                if((i.first-'a') == j) {
-                    if(prefix[high][j]-prefix[low][j] > 2) ans++;
+                if(prefix[j] && suffix[j] && mpp[s[i]].find(j) == mpp[s[i]].end()) {
+                    mpp[s[i]].insert(j);
+                    ans++;
                 }
-                else if(prefix[high][j]-prefix[low][j] > 0) ans++;
             }
+            prefix[s[i]-'a']++;
         }
         return ans;
     }
