8637be76cb3f11d1b800a5e0a0909a2a8eee10a4
Ashish Shivhare <aashishshivhare97@gmail.com>
2025-11-30 12:08:21 +0530

    Time: 25 ms (31.82%), Space: 25.3 MB (9.09%) - LeetHub

---
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxDistinct(string s) {
+        unordered_set<char> st;
+        for(auto i: s) {
+            st.insert(i);
+        }
+        return st.size();
+    }
+};
