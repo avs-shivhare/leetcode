18c8406392f97ec0091a35d8e3fa5fedc0309f5a
Ashish Shivhare <aashishshivhare97@gmail.com>
2025-12-23 20:27:15 +0530

    Time: 327 ms (5.41%), Space: 199.8 MB (10.42%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-    int find(vector<vector<int>> &en,int val) {
-        int n = en.size();
-        int ans = n;
-        int l =0,r = n-1;
-        while(l<=r) {
-            int mid = (l+r)>>1;
-            if(en[mid][0] > val) {
-                ans = mid;
-                r = mid-1;
-            }
-            else l = mid+1;
-        }
-        return ans;
-    }
-    int maxTwoEvents(vector<vector<int>>& en) {
-        sort(en.begin(),en.end());
-        int n = en.size();
-        int suffix[100001];
-        memset(suffix,0,sizeof(suffix));
-        suffix[n-1] = en[n-1][2];
-        for(int i = n-2; i>=0; i--) {
-            suffix[i] = max(en[i][2],suffix[i+1]);
+    int maxTwoEvents(vector<vector<int>>& events) {
+        sort(events.begin(),events.end(),[&](auto &a,auto &b) {
+            if(a[1] == b[1]) return a[0] < b[0];
+            return a[1] < b[1];
+        });
+        priority_queue<vector<int>> q;
+        for(auto &i: events) {
+            q.push({i[2],i[0],i[1]});
         }
         int ans = 0;
-        for(int i = 0; i<n; i++) {
-            int x = find(en,en[i][1]);
-            if(x != n) ans = max(ans,suffix[x]+en[i][2]);
-            else ans = max(ans,en[i][2]);
+        for(auto &i: events) {
+            while(!q.empty() && q.top()[1] <= i[1]) q.pop();
+            if(!q.empty()) ans = max(ans,i[2]+q.top()[0]);
+            ans = max(ans,i[2]);
         }
         return ans;
     }
