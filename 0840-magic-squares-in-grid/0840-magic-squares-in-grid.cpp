668b834cf18d789c6c8504a40d151e39cec652c0
Ashish Shivhare <aashishshivhare97@gmail.com>
2025-12-31 02:07:53 +0530

    Time: 0 ms (100%), Space: 12.1 MB (43.03%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.cpp
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int cnt = 0;
         int n = grid.size();
         int m = grid[0].size();
-        int cnt =0;
-        for(int i =0; i+2<n; i++) {
-            for(int j =0; j+2<m; j++) {
-                bool flag = true;
-                int row = 0;
+        for(int i = 0; i+3<=n; i++) {
+            for(int j = 0; j+3<=m; j++) {
                 unordered_map<int,int> mpp;
-                for(int x =0; x<3; x++) {
-                    int temp = 0;
-                    for(int y =0; y<3; y++) {
-                        temp += grid[i+x][j+y];
-                        if(grid[i+x][j+y] < 1 || grid[i+x][j+y] > 9) flag = false;
-                        if(mpp.count(grid[i+x][j+y])) flag = false;
+                bool flag = true;
+                for(int x = 0; x<3; x++) {
+                    for(int y = 0; y<3; y++) {
+                        if(mpp[grid[i+x][j+y]] || grid[i+x][j+y] < 1 || grid[i+x][j+y] > 9) {
+                            flag = false;
+                            break;
+                        }
                         mpp[grid[i+x][j+y]]++;
                     }
-                    if(row != 0 && temp != row) flag = false;
-                    row = temp;
-                    temp = 0;
-                    for(int y =0; y<3; y++) {
-                        temp += grid[i+y][j+x];
-                    }
-                    if(row != temp) flag = false;
+                    if(!flag) break;
                 }
-                int dig1 = grid[i][j]+grid[i+1][j+1]+grid[i+2][j+2];
-                int dig2 = grid[i+2][j]+grid[i+1][j+1]+grid[i][j+2];
-                if(row != dig1 || row != dig2) flag = false;
-                if(flag) cnt++;
-                cout<<row<<endl;
+                if(!flag) continue;
+                int r1 = grid[i][j]+grid[i][j+1]+grid[i][j+2];
+                int r2 = grid[i+1][j]+grid[i+1][j+1]+grid[i+1][j+2];
+                int r3 = grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2];
+                int c1 = grid[i][j]+grid[i+1][j]+grid[i+2][j];
+                int c2 = grid[i][j+1]+grid[i+1][j+1]+grid[i+2][j+1];
+                int c3 = grid[i][j+2]+grid[i+1][j+2]+grid[i+2][j+2];
+                int d1 = grid[i][j]+grid[i+1][j+1]+grid[i+2][j+2];
+                int d2 = grid[i+2][j]+grid[i+1][j+1]+grid[i][j+2];
+                if(r1 == r2 && r2 == r3 && r3 == c1 && c1 == c2 && c2 == c3 && c3 == d1 && d1 == d2) cnt++;
             }
         }
         return cnt;
