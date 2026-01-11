80f66ffd13d842fc51a12fa9bba429bf71ff2384
Ashish Shivhare <aashishshivhare97@gmail.com>
2026-01-11 12:47:26 +0530

    Time: 0 ms (100%), Space: 18.7 MB (53.71%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-    int maxHeight(vector<int> height) {
-        int n = height.size();
-        stack<int> st1,st2;
-        vector<int> left(n,0),right(n,0);
-        for(int i =0; i<n; i++) {
-            while(!st1.empty() && height[st1.top()] >= height[i]) {
-                st1.pop();
-            }
-            if(st1.empty()) left[i] = 0;
-            else left[i] = st1.top()+1;
-            st1.push(i);
+    int find(vector<int> &arr) {
+        int n = arr.size();
+        vector<int> left(n);
+        stack<int> st;
+        for(int i = 0; i<n; i++) {
+            while(!st.empty() && arr[st.top()]>= arr[i]) st.pop();
+            if(!st.empty()) left[i] = st.top()+1;
+            st.push(i);
         }
+        while(!st.empty()) st.pop();
         int ans = 0;
-        for(int i =n-1; i>=0; i--) {
-            while(!st2.empty() && height[st2.top()] >= height[i]) st2.pop();
-            if(st2.empty()) right[i] = n-1;
-            else right[i] = st2.top()-1;
-            st2.push(i);
-            ans = max(ans,(right[i]-left[i]+1)*height[i]);
+        for(int i = n-1; i>=0; i--) {
+            while(!st.empty() && arr[st.top()] >= arr[i]) st.pop();
+            int r = n-1;
+            if(!st.empty()) r = st.top()-1;
+            st.push(i);
+            ans = max(ans,arr[i]*(r-left[i]+1));
         }
         return ans;
     }
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<int> height(m,0);
-        int maxi = 0;
-        for(int i =0; i<n; i++) {
-            for(int j =0; j<m; j++) {
-                if(matrix[i][j] == '1') height[j]++;
-                else height[j] = 0;
+    int maximalRectangle(vector<vector<char>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int ans = 0;
+        vector<int> arr(m,0);
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<m; j++) {
+                if(mat[i][j] == '1') arr[j]++;
+                else arr[j] = 0;
             }
-            maxi =max(maxi,maxHeight(height));
+            ans = max(ans,find(arr));
         }
-        return maxi;
+        return ans;
     }
 };
