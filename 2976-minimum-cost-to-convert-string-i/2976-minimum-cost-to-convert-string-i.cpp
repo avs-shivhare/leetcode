347f54ab82f6170688f031e2b0040e821abf5555
Ashish Shivhare <aashishshivhare97@gmail.com>
2026-01-29 22:25:15 +0530

    Time: 82 ms (19.4%), Space: 125.1 MB (12.94%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
-    long long minimumCost(string sr, string tr, vector<char>& org, vector<char>& ch, vector<int>& cost) {
-        vector<vector<long long>> dist(26,vector<long long>(26,1e12));
-        long long ans =0;
-        int n = org.size();
-        for(int i =0; i<n; i++) {
-            dist[org[i]-'a'][ch[i]-'a'] = min(dist[org[i]-'a'][ch[i]-'a'],(long long)cost[i]);
-        }
-        for(int i =0; i<26; i++) dist[i][i] =0;
-        for(int k =0; k<26; k++) {
-            for(int i =0; i<26; i++) {
-                for(int j =0; j<26; j++) {
-                    dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j]);
+    vector<long long> find(int node,vector<vector<long long>> adj[]) {
+        vector<long long> dist(26,LLONG_MAX);
+        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>> q;
+        dist[node] = 0;
+        q.push({0,node});
+        while(!q.empty()) {
+            int n = q.top().second;
+            auto cost = q.top().first;
+            q.pop();
+            if(dist[n] < cost) continue;
+            for(auto &i: adj[n]) {
+                if(dist[i[0]] > cost+i[1]) {
+                    dist[i[0]] = cost+i[1];
+                    q.push({dist[i[0]],i[0]});
                 }
             }
         }
-        n = sr.size();
-        for(int i =0; i<n; i++) {
-            if(dist[sr[i]-'a'][tr[i]-'a'] >= 1e12) return -1;
-            ans += dist[sr[i]-'a'][tr[i]-'a'];
+        for(int i = 0; i<26; i++) {
+            if(dist[i] >= LLONG_MAX) dist[i] = -1;
+        }
+        return dist;
+    }
+    long long minimumCost(string s, string t, vector<char>& org, vector<char>& ch, vector<int>& cost) {
+        vector<vector<long long>> adj[26];
+        int m = org.size();
+        int n = s.size();
+        for(int i = 0; i<m; i++) {
+            adj[org[i]-'a'].push_back({ch[i]-'a',cost[i]});
+        }
+        vector<vector<long long>> dist(26);
+        for(int i = 0; i<26; i++) {
+            dist[i] = find(i,adj);
+        }
+        long long ans = 0;
+        for(int i = 0; i<n; i++) {
+            if(dist[s[i]-'a'][t[i]-'a'] == -1) return -1;
+            ans += dist[s[i]-'a'][t[i]-'a'];
         }
         return ans;
     }
