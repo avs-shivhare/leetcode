49ed120cfbf67b04569bc4a5961721aca3f9bf31
Ashish Shivhare <aashishshivhare97@gmail.com>
2026-02-07 12:23:32 +0530

    Time: 24 ms (60.5%), Space: 25.5 MB (72.34%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         int n = s.size();
-        vector<int> A(n,0),B(n,0);
-        int cnt =0;
-        for(int i =0; i<n; i++) {
-            B[i]=cnt;
-            if(s[i] == 'b') cnt++;
+        //if(n == 1) return 0;
+        int a = 0,b = 0;
+        for(int i = 0; i<n; i++) {
+            if(s[i] == 'a') a++;
         }
-        cnt =0;
-        for(int i =n-1; i>=0; i--) {
-            A[i] = cnt;
-            if(s[i] == 'a') cnt++;
+        //if(a == 0 || a == n) return 0;
+        int ans = 1e9;
+        for(int i = 0; i<n; i++) {
+            ans = min(ans,a+b);
+            //cout<<a<<" "<<b<<" "<<i<<endl;
+            if(s[i] == 'a') a--;
+            else b++;
+            ans = min(ans,a+b);
         }
-        int mini =1e9;
-        for(int i =0; i<n; i++) {
-            mini = min(mini,A[i]+B[i]);
-        }
-        return mini;
+        return ans;
     }
 };
