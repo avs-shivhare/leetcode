2fb71d7531c2bd4df8e404361e98bde90e10ad04
Ashish Shivhare <aashishshivhare97@gmail.com>
2026-02-22 13:39:47 +0530

    Time: 0 ms (100%), Space: 8.1 MB (44.66%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int last = -1;
+        int ans = 0;
+        for(int i = 0; i<32; i++) {
+            if(n&(1<<i)) {
+                if(last != -1) {
+                    ans = max(ans,i-last);
+                }
+                last = i;
+            }
+        }
+        return ans;
+    }
+};
