a549a096f0b0283929f6d927eab4ff1c9f54da8e
Ashish Shivhare <aashishshivhare97@gmail.com>
2026-04-05 23:23:52 +0530

    Time: 3 ms (46.4%), Space: 10.4 MB (47.26%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0,y = 0;
+        for(auto &i: moves) {
+            if(i == 'L') x--;
+            else if(i == 'R') x++;
+            else if(i == 'U') y++;
+            else y--;
+        }
+        return x == 0 && y == 0;
+    }
+};
