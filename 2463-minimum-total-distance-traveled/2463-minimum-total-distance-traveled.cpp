ac416cc24493cfc9f695f50e3bb237426e174335
Ashish Shivhare <aashishshivhare97@gmail.com>
2026-04-15 00:47:53 +0530

    Time: 33 ms (58.9%), Space: 11.7 MB (91.1%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    long long dp[101][101][101];
-    long long find(int i,int j,int cnt,vector<int> &robot,vector<vector<int>> &fact) {
-        if(i == robot.size()) return 0;
-        if(j == fact.size()) return 1e18;
-        if(dp[i][j][cnt] != -1) return dp[i][j][cnt];
-        long long take = 1e18,notTake = 1e18;
-        if(fact[j][1] > cnt) {
-            //cout<<robot[i]<<" "<<fact[j][0]<<" abs "<<i<<" "<<j<<endl;
-            take = abs(robot[i]-fact[j][0])+find(i+1,j,cnt+1,robot,fact);
+    long long dp[101][101];
+    long long find(int i,int j,vector<int> &r,vector<vector<int>> &f) {
+        if(i >= r.size()) return 0ll;
+        if(j >= f.size()) return 1e18;
+        if(dp[i][j] != -1) return dp[i][j];
+        long long ans = 1e18;
+        long long sum = 0;
+        ans = min(ans,0+find(i,j+1,r,f));
+        for(int x = i; x<r.size() && x-i < f[j][1]; x++) {
+            sum += abs(1ll*r[x]-f[j][0]);
+            ans = min(ans,sum+find(x+1,j+1,r,f));
         }
-        notTake = find(i,j+1,0,robot,fact);
-        //cout<<i<<" "<<j<<" "<<take<<" "<<notTake<<endl;
-        return dp[i][j][cnt] = min(take,notTake);
+        return dp[i][j] = ans;
     }
-    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& fact) {
+    long long minimumTotalDistance(vector<int>& r, vector<vector<int>>& f) {
+        sort(r.begin(),r.end());
+        sort(f.begin(),f.end());
         memset(dp,-1,sizeof(dp));
-        sort(robot.begin(),robot.end());
-        sort(fact.begin(),fact.end());
-        return find(0,0,0,robot,fact);
+        return find(0,0,r,f);
     }
 };
