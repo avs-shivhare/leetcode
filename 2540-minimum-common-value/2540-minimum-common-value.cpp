5ebc7b5d0099c7507bb252993541f0fabb3cecfd
Ashish Shivhare <aashishshivhare97@gmail.com>
2026-05-20 00:51:35 +0530

    Time: 3 ms (38.92%), Space: 54.4 MB (97.72%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
+    bool find(vector<int> &nums,int &el) {
+        int l = 0,r = nums.size()-1;
+        while(l<=r) {
+            int mid = (l+r)>>1ll;
+            if(nums[mid] == el) return true;
+            else if(nums[mid] < el) l = mid+1;
+            else r = mid-1;
+        }
+        return false;
+    }
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        int i =0,j =0;
-        while(i<n && j<m) {
-            if(nums1[i] == nums2[j]) return nums1[i];
-            else if(nums1[i] < nums2[j]) {
-                i++;
-            }
-            else {
-                j++;
-            }
+        for(auto &i: nums1) {
+            if(find(nums2,i)) return i;;
         }
         return -1;
     }
