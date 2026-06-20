655e1dc617e2e589d2eb288d322c400a0063fb84
Ashish Shivhare <aashishshivhare97@gmail.com>
2026-06-21 00:31:12 +0530

    Time: 51 ms (80.56%), Space: 106.7 MB (38.89%) - LeetHub

---
+++ 1840-maximum-building-height/1840-maximum-building-height.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int maxBuilding(int n, vector<vector<int>>& res) {
+        res.push_back({1,0});
+        res.push_back({n,n-1});
+        sort(res.begin(),res.end());
+        long long ans = 0;
+        int m = res.size();
+        for(int i = 1; i<m; i++) {
+            if(res[i][1] > res[i-1][1]) {
+                res[i][1] = min(res[i][1],res[i-1][1]+(res[i][0]-res[i-1][0]));
+            }
+        }
+        for(int i = m-2; i>=0; i--) {
+            if(res[i][1] > res[i+1][1]) {
+                res[i][1] = min(res[i][1],res[i+1][1]+(res[i+1][0]-res[i][0]));
+            }
+        }
+        // for(auto i: res) cout<<i[1]<<" ";
+        // cout<<endl;
+        for(int i = 1; i<m; i++) {
+            long long diff = 1ll*res[i][0]-res[i-1][0];
+            long long diffh = abs(1ll*res[i-1][1]-res[i][1]);
+            if(diff < diffh) {
+                //cout<<" first "<<endl;
+                res[i][1] = res[i-1][1]+diff;
+                ans = max(1ll*res[i][1],ans);
+            }
+            else {
+                diff -= diffh;
+                ans = max(ans,max(res[i][1],res[i-1][1])+(diff>>1));
+            }
+            //cout<<ans<<endl;
+        }
+        // long long diff = 1ll*n-res.back()[0];
+        // //cout<<diff<<endl;
+        // ans = max(ans,res.back()[1]+diff);
+        return ans;
+    }
+};
