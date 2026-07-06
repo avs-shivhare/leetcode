923d8fcbe272ca64aed7de4ab616daa746b633ca
Ashish Shivhare <aashishshivhare97@gmail.com>
2026-07-06 11:35:14 +0530

    Time: 4 ms (28.48%), Space: 15.9 MB (14.83%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int removeCoveredIntervals(vector<vector<int>>& inter) {
-        sort(inter.begin(),inter.end());
-        int n = inter.size();
-        int ans = 0;
-        int i =0;
-        while(i<n) {
-            int j = i+1;
-            int start = inter[i][0];
-            int end = inter[i][1];
-            while(j<n && ((start <= inter[j][0] && inter[j][1] <= end) || (inter[j][0] <= start && end <= inter[j][1]))) {
-                start = min(start,inter[j][0]);
-                end = max(end,inter[j][1]);
-                j++;
-            }
-            i =j;
-            ans++;
+    int removeCoveredIntervals(vector<vector<int>>& arr) {
+        sort(arr.begin(),arr.end(),[&](auto &a,auto &b){
+            if(a[1] != b[1]) return a[1] < b[1];
+            return a[0] <= b[0];
+        });
+        vector<vector<int>> temp;
+        for(auto &i: arr) {
+            while(!temp.empty() && i[0] <= temp.back()[0] && temp.back()[1] <= i[1]) temp.pop_back();
+            if(temp.empty() || !(temp.back()[0] <= i[0] & i[1] <= temp.back()[1])) temp.push_back(i);
         }
-        return ans;
+        return temp.size();
     }
 };
