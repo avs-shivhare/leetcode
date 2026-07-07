f48901a98d087d8acebaa433697ef8f903651353
Ashish Shivhare <aashishshivhare97@gmail.com>
2026-07-07 11:26:56 +0530

    Time: 0 ms (100%), Space: 9.3 MB (27.38%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     long long sumAndMultiply(int n) {
-        string s = to_string(n);
-        string temp = "";
         long long sum = 0;
-        for(auto i: s) {
-            if(i != '0') {
-                temp += i;
-                sum += i-'0';
-            }
-        }
-        if(temp.empty()) return 0;
-        long long ans = stoll(temp)*sum;
-        return ans;
+        string s = to_string(n);
+        string ans = "";
+        for(auto &i: s) {
+            if(i != '0') ans += i;
+            sum += i-'0';
+        } 
+        if(ans.empty()) return 0;
+        return sum*stoll(ans);
     }
 };
