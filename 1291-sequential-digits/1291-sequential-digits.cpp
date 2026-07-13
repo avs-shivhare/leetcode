a40f31e0a1e5533a84c937d46c23ca31e702ace8
Ashish Shivhare <aashishshivhare97@gmail.com>
2026-07-13 13:48:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (5.54%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,50 +1,26 @@
 class Solution {
 public:
-    
+    vector<int> ans;
+    string temp ="";
+    void find(int i,int &low,int &high,int prev) {
+        if(!temp.empty()) {
+            //cout<<temp<<endl;
+            long long t = stoll(temp);
+            if(t >= low && t <= high) ans.push_back(t);
+            if(t > high) return;
+        }
+        if(prev < 9) {
+            temp.push_back(prev+1+'0');
+            find(i+1,low,high,prev+1);
+            temp.pop_back();
+        }
+        return;
+    }
     vector<int> sequentialDigits(int low, int high) {
-        string s = to_string(low);
-        int n = s.size();
-        vector<int> ans;
-        long long temp = 0;
-        char ch = s[0];
-        do {
-            if(s[0] == '9') {
-                s = "";
-                for(int i =1; i<=n+1; i++) {
-                    s.push_back(i+'0');
-                }
-                n++;
-            }
-            else {
-                string temp = "";
-                for(int i =0; i<n; i++) {
-                    temp.push_back(ch+i);
-                }
-                s = temp;
-                ch = s[0]+1;
-            }
-            if(n > 9) return ans;
-            temp = stoul(s);
+        for(int i = 0; i<9; i++) {
+            find(0,low,high,i);
         }
-        while(temp < low);
-        while(temp <= high) {
-            ans.push_back(temp);
-            //cout<<s<<endl;
-            if(s[n-1] == '9') {
-                s = "";
-                for(int i =1; i<=n+1; i++) {
-                    s.push_back(i+'0');
-                }
-                n++;
-            }
-            else {
-                s.erase(s.begin());
-                //cout<<s[n-2]+1<<endl;
-                s.push_back(s[n-2]+1);
-            }
-            if(n > 9) return ans;
-            temp = stoul(s);
-        }
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
