7fe43144b3a560696f48b2e96737a8f0d220872a
Ashish Shivhare <aashishshivhare97@gmail.com>
2026-07-16 12:36:39 +0530

    Time: 88 ms (12.6%), Space: 161.1 MB (57.76%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
+    long long find(long long a,long long b) {
+        if(a == 0) return b;
+        if(b == 0) return a;
+        if(a > b) return find(a%b,b);
+        return find(a,b%a);
+    }
     long long gcdSum(vector<int>& nums) {
         int n = nums.size();
         vector<long long> prefix(n,0);
-        long long maxi = 0;
+        long long maxi = -1e18;
         for(int i = 0; i<n; i++) {
-            maxi = max(maxi,1ll*nums[i]);
-            prefix[i] = gcd(nums[i]*1ll,maxi);
+            maxi = max(1ll*nums[i],maxi);
+            prefix[i] = find(nums[i],maxi);
         }
         sort(prefix.begin(),prefix.end());
-        long long sum = 0;
         int l = 0,r = n-1;
+        long long ans = 0;
         while(l<r) {
-            sum += gcd(prefix[l],prefix[r]);
+            ans += find(prefix[l],prefix[r]);
             l++;
             r--;
         }
-        return sum;
+        return ans;
     }
 };
