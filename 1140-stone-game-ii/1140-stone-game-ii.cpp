ef5fc8688d0f289d7b1daedadabf166e297cc4a2
Ashish Shivhare <aashishshivhare97@gmail.com>
2026-08-09 19:20:37 +0530

    Time: 13 ms (39.31%), Space: 11.2 MB (81.19%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    int dp[101][101];
-    int find(int i,int m,vector<int> &piles,int &n) {
-        if(i >= n) return 0;
-        if(dp[i][m] != -1) return dp[i][m];
-        int sum =0;
-        int ans = INT_MIN;
-        for(int x = 0 ; x<2*m && x+i < n; x++) {
-            sum += piles[x+i];
-            ans = max(ans,sum - find(i+x+1,max(m,x+1),piles,n));
+    int dp[101][201][2];
+    int find(int i,int m,int t,vector<int> &piles) {
+        if(i >= piles.size()) return 0;
+        if(dp[i][m][t] != -1) return dp[i][m][t];
+        int ans = 0;
+        if(t) ans = 1e9;
+        int sum = 0;
+        for(int j = 0; j<2*m && j+i < piles.size(); j++) {
+            sum += piles[i+j];
+            if(t) ans = min(ans,find(i+j+1,max(m,j+1),0,piles));
+            else ans = max(ans,sum+find(i+j+1,max(m,j+1),1,piles));
         }
-        return dp[i][m] = ans;
+        return dp[i][m][t] = ans;
     }
     int stoneGameII(vector<int>& piles) {
         memset(dp,-1,sizeof(dp));
-        int n = piles.size();
-        int ans =  find(0,1,piles,n);
-        int sum = 0;
-        for(auto i:piles) {
-            sum += i;
-        }
-        return (sum+ans)/2;
+        return find(0,1,0,piles);
     }
 };
